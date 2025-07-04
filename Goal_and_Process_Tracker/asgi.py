a854448d55082881ef2de8de666e4c42ae772c91
lukasz <lucapiasek@gmail.com>
2025-07-04 20:22:40 +0200

    Create Goal and Process Tracker Project

---
+++ Goal_and_Process_Tracker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Goal_and_Process_Tracker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Goal_and_Process_Tracker.settings')
+
+application = get_asgi_application()
---
+++ Goal_and_Process_Tracker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Goal_and_Process_Tracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Goal_and_Process_Tracker.settings')
+
+application = get_wsgi_application()
