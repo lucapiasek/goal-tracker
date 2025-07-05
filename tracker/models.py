31cd62080f8e9d851bdc246877ebb2c4d60504d7
lukasz <lucapiasek@gmail.com>
2025-07-05 20:09:35 +0200

    Create goal and piece models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Goal(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    pieces = models.ManyToManyField("Piece", blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    is_realized = models.BooleanField(default=False)
+
+class Piece(models.Model):
+    goal = models.ManyToManyField("Goal", blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    name_to_display = models.CharField(max_length=200, blank=True, null=True)
+    author = models.CharField(max_length=200, blank=True, null=True)
+    opus = models.CharField(max_length=30, blank=True, null=True)
+    number = models.CharField(max_length=30, blank=True, null=True)
+    color = models.CharField(max_length=60, blank=True, null=True)
+    genre = models.CharField(max_length=60, blank=True, null=True)
+    period = models.CharField(max_length=100, blank=True, null=True)
+    pitch = models.CharField(max_length=30, blank=True, null=True)
+    type = models.CharField(max_length=70, blank=True, null=True)
+    time_to_master = models.DurationField(blank=True, null=True, help="Sugerowany czas opanowania utworu")
+    is_mastered = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+
+
