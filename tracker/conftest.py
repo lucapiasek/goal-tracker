faba12486c91dfde3d7a2c6770d1fba7f44771e6
lukasz <lucapiasek@gmail.com>
2025-07-19 14:44:23 +0200

    test: add goal fixture in conftest

--- tracker/conftest.py
+++ tracker/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from django.test import Client
+from tracker.factories import GoalFactory
 
 @pytest.fixture
 def client():
     return Client()
+
+@pytest.fixture
+def goal():
+    return GoalFactory()
