071f107e0fe2b8fdb920be74218a6f542a2cf701
lukasz <lucapiasek@gmail.com>
2025-07-21 19:42:14 +0200

    feat: register pieces view in urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'tracker'
 urlpatterns = [
     path('goals/', views.GoalsView.as_view(), name='goals'),
+
+    path('pieces/', views.PiecesView.as_view(), name='pieces')
 ]
