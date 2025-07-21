d0ba0aaab31d11b82e37e090aad8f18ffde7974a
lukasz <lucapiasek@gmail.com>
2025-07-21 19:41:40 +0200

    feat: add pieces view

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import Goal
+from .models import Goal, Piece
 from django.views.generic import ListView
+from django.views import View
 
 class GoalsView(ListView):
     template_name = "tracker/goals.html"
     model = Goal
+
+class PiecesView(View):
+    def get(self, request):
+        query_set = Piece.objects.all()
+        return render(request, 'tracker/pieces.html', {'piece_list': query_set})
