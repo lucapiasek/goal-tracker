c88ee2680706a93671600467db297dbc71811d4e
lukasz <lucapiasek@gmail.com>
2025-07-26 06:57:05 +0200

    test: add Piece factory

--- tracker/factories.py
+++ tracker/factories.py
@@ -1,8 +1,14 @@
 import factory
-from tracker.models import Goal
+from tracker.models import Goal, Piece
 
 class GoalFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Goal
 
     name = factory.Faker('sentence')
+
+class PieceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Piece
+
+    name = factory.Faker('name')
