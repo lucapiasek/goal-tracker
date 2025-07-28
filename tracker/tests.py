a161ee7dcbc59bce4d4e30f9e7cd7030e7daaaa4
lukasz <lucapiasek@gmail.com>
2025-07-29 00:13:19 +0200

    test: add missing pytest django_db fixture to goal detail view test

--- tracker/tests.py
+++ tracker/tests.py
@@ -52,6 +52,7 @@ def test_goal_view_returns_404_with_non_existent_goal(client):
     response = client.get(url)
     assert response.status_code == 404
 
+@pytest.mark.django_db
 def test_goal_detail_view(client, goal):
     url = reverse('tracker:goal', args=[goal.pk])
     response = client.get(url)
