46004ed40a023308a82f43473b69cc486351239a
lukasz <lucapiasek@gmail.com>
2025-07-29 00:08:37 +0200

    feat: add goal detail view to views

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Goal, Piece
 from django.views.generic import ListView
 from django.views import View
@@ -6,6 +6,11 @@ from django.views import View
 class GoalsView(ListView):
     template_name = "tracker/goals.html"
     model = Goal
+
+class GoalDetailView(View):
+    def get(self, request, pk):
+        goal = get_object_or_404(Goal, pk=pk)
+        return render(request, 'tracker/goal.html', {'goal': goal})
 
 class PiecesView(View):
     def get(self, request):
