237ba15cf7d7158d58611db51d26ff9046499f99
lukasz <lucapiasek@gmail.com>
2025-07-29 21:06:54 +0200

    feat: add title property to goal create form

--- tracker/forms.py
+++ tracker/forms.py
@@ -2,6 +2,10 @@ from django.forms import ModelForm
 from tracker.models import Goal
 
 class GoalCreateForm(ModelForm):
+    @property
+    def title(self):
+        return f"{self._meta.model._meta.verbose_name.title()}"
+
     class Meta:
         model = Goal
         fields = "__all__"
