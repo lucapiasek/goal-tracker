71c39057710cebceabfced67d3183da7121fb840
lukasz <lucapiasek@gmail.com>
2025-07-29 18:56:26 +0200

    feat: add create goal view to urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,5 +5,6 @@ app_name = 'tracker'
 urlpatterns = [
     path('goals/', views.GoalsView.as_view(), name='goals'),
     path('goal/<int:pk>', views.GoalDetailView.as_view(), name='goal'),
+    path('goal/new', views.GoalCreateView.as_view(), name='goal-create'),
     path('pieces/', views.PiecesView.as_view(), name='pieces')
 ]
