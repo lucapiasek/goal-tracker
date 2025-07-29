ef3502502ffd8b0cbf20f4b96385cf9ca5fbdeaa
lukasz <lucapiasek@gmail.com>
2025-07-29 18:55:44 +0200

    feat: add goal create view get and post

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Goal, Piece
+from .forms import GoalCreateForm
 from django.views.generic import ListView
 from django.views import View
 
@@ -11,6 +12,18 @@ class GoalDetailView(View):
     def get(self, request, pk):
         goal = get_object_or_404(Goal, pk=pk)
         return render(request, 'tracker/goal.html', {'goal': goal})
+
+class GoalCreateView(View):
+    def get(self, request):
+        form = GoalCreateForm()
+        return render(request, 'tracker/create_form.html', {'form': form})
+
+    def post(self, request):
+        form = GoalCreateForm(request.POST)
+        if form.is_valid():
+            goal = form.save()
+            return redirect('tracker:goals')
+        return render(request, 'tracker/create_form.html', {'form': form})
 
 class PiecesView(View):
     def get(self, request):
