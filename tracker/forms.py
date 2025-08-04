cb63019914d646612d6d3aae4c237f06461092cf
lukasz <lucapiasek@gmail.com>
2025-08-04 13:26:40 +0200

    refactor: change goal create form title property to implicit string

--- tracker/forms.py
+++ tracker/forms.py
@@ -4,8 +4,9 @@ from tracker.models import Goal
 class GoalCreateForm(ModelForm):
     @property
     def title(self):
-        return f"{self._meta.model._meta.verbose_name.title()}"
+        return "Cel"
 
     class Meta:
         model = Goal
         fields = "__all__"
+
