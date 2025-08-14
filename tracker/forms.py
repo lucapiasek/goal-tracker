a799c3db2a973c9cdd006de6d0ea456b4963480b
lukasz <lucapiasek@gmail.com>
2025-08-14 20:46:20 +0200

    refactor: change GoalCreateView to Form based View, add validation to prevent empty models

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,6 +1,42 @@
-from django.forms import Form
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Piece
 
-class GoalCreateForm(Form):
+date_input_formats = [
+        "%d.%m.%Y",
+        "%d.%m.%y",
+        "%d/%m/%Y",
+        "%d/%m/%y",
+        "%d-%m-%Y",
+        "%d-%m-%y",
+        "%d %m %Y",
+        "%d %m %y",
+        "%Y/%m/%d",
+        "%Y.%m.%d"
+    ]
+
+class GoalCreateForm(forms.Form):
+    name = forms.CharField(max_length=200, label='Cel', required=False, empty_value='')
+    piece = forms.CharField(max_length=200, label='Utwór', required=False, empty_value='')
+    pieces = forms.ModelMultipleChoiceField(queryset=Piece.objects.all(), label="Utwory", required=False, widget=forms.CheckboxSelectMultiple)
+    date = forms.DateField(required=False, label='Data', input_formats=date_input_formats)
+    time = forms.TimeField(label="Godz.", required=False)
+    additional_info = forms.CharField(label="Dodatkowe informacje", required=False, )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        piece = cleaned_data.get("piece")
+        pieces = cleaned_data.get("pieces")
+        additional_info = cleaned_data.get("additional_info")
+
+        if not name and not piece and not pieces.exists() and not additional_info:
+            err = ValidationError("Conajmniej jedno z tych pól musi być wypełnione")
+            self.add_error("name", err)
+            self.add_error("piece", err)
+            self.add_error("pieces", err)
+            self.add_error("additional_info", err)
+
     @property
     def title(self):
         return "Cel"
