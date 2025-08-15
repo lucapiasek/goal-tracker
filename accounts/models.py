3ded77cc70ec723e4e02abcf3e829801837b0424
lukasz <lucapiasek@gmail.com>
2025-08-15 20:36:54 +0200

    fix: remove blank True in accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.conf import settings
 
 class Student(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_student = models.BooleanField(default=True)
 
 class Teacher(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     students = models.ManyToManyField(Student, blank=True, related_name="teachers")
     is_teacher =  models.BooleanField(default=True)
