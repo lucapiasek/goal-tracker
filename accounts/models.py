37cc8abd55d762ec47b1bff39d7639d2acc135e7
lukasz <lucapiasek@gmail.com>
2025-08-15 20:33:32 +0200

    feat: add Student and Teacher models in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
+    is_student = models.BooleanField(default=True)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, blank=True, related_name="teachers")
+    is_teacher =  models.BooleanField(default=True)
