1aafcb3f0bd6d1f8a1c8244e8ee86cac6f08378e
lukasz <lucapiasek@gmail.com>
2025-08-16 02:59:41 +0200

    feat: add login view in accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('user/create/', views.UserCreateView.as_view(), name='create'),
+    path('user/login', LoginView.as_view(
+        template_name='accounts/create_form.html',
+        next_page='tracker:goals',
+        extra_context={'page_title': "Zaloguj się"}
+    ), name='login'),
 ]
