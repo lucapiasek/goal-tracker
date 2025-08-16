efc5d75cc968db0d35f565f3464d1dadac1d2923
lukasz <lucapiasek@gmail.com>
2025-08-16 02:26:46 +0200

    feat: add view for creating user in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+class UserCreateView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'accounts/create_form.html', {'form': form, 'page_title': "Zarejestruj się"})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tracker:goals')
+        return render(request, 'accounts/create_form.html', {'form': form, 'page_title': "Zarejestruj się"})
