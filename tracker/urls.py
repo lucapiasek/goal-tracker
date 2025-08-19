86489f9fef8f75946191852311ab791d17d77ffe
lukasz <lucapiasek@gmail.com>
2025-08-19 22:57:48 +0200

    feat: add username argument to goal create view url

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,6 +5,7 @@ app_name = 'tracker'
 urlpatterns = [
     path('<str:username>/goals/', views.GoalsView.as_view(), name='goals'),
     path('<str:username>/goal/<int:pk>/', views.GoalDetailView.as_view(), name='goal'),
-    path('goal/new', views.GoalCreateView.as_view(), name='goal-create'),
+    path('<str:username>/goal/new', views.GoalCreateView.as_view(), name='goal-create'),
+    path('<str:username>/goal/<int:pk>/update', views.GoalUpdateView.as_view(), name='goal_update'),
     path('pieces/', views.PiecesView.as_view(), name='pieces')
 ]
