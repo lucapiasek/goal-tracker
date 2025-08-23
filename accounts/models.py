e197242cb15102018c4fd35d82b8f8a135f18965
lukasz <lucapiasek@gmail.com>
2025-08-23 20:05:38 +0200

    feat: add teacher invitations field for student model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.conf import settings
 
 class Student(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='student')
     is_student = models.BooleanField(default=False)
+    teacher_invitations = models.ManyToManyField('Teacher', related_name='student_invitations')
 
 class Teacher(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher')
     students = models.ManyToManyField(Student, blank=True, related_name="teachers")
     is_teacher =  models.BooleanField(default=False)
