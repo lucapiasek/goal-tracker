2a621d7325385b692cb97c2c29376ef783bd1cbf
lukasz <lucapiasek@gmail.com>
2025-08-29 20:07:24 +0200

    test: add task with goal and task with piece fixtures in tasks conftest

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,7 +1,8 @@
 import pytest
 from django.test import Client
 from django.contrib.auth import get_user_model
-from tracker.models import Task, Practice
+from tracker.models import Task, Practice, Goal, Piece
+from factory import Faker
 
 UserModel = get_user_model()
 
@@ -13,3 +14,18 @@ def user():
 def user2():
     return UserModel.objects.create_user(username="test2", password="password")
 
+@pytest.fixture
+def goal(user):
+    return UserModel.objects.create(user=user, name=Faker("sentence"))
+
+@pytest.fixture
+def piece(user):
+    return UserModel.objects.create(user=user, name_to_display=Faker("name"))
+
+@pytest.fixture
+def task_with_goal(user, goal):
+    return Task.objects.create(user=user, goal=goal)
+
+@pytest.fixture
+def task_with_piece(user, piece):
+    return Task.objects.create(user=user, piece=piece)
