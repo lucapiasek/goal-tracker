02d3842eb389d01130cf8d531bcd7aa48aa8726e
lukasz <lucapiasek@gmail.com>
2025-08-29 20:01:31 +0200

    test: add users to conftest in tasks app

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from django.contrib.auth import get_user_model
+from tracker.models import Task, Practice
 
-# Create your tests here.
+UserModel = get_user_model()
+
+@pytest.fixture
+def user():
+    return UserModel.objects.create_user(username="test2", password="password")
+
+@pytest.fixture
+def user2():
+    return UserModel.objects.create_user(username="test2", password="password")
+
