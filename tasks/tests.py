6fa91e8dad8b64af0ee06e5f2cbb54576e000f98
lukasz <lucapiasek@gmail.com>
2025-08-30 15:06:09 +0200

    test: add task list view provides user's tasks only test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-
+from tracker.models import Goal, Task
 
 @pytest.mark.django_db
 def test_task_list_view(client, user, logged):
@@ -26,9 +26,22 @@ def test_task_list_view_with_task(client, user, logged, goal_task):
 @pytest.mark.django_db
 def test_task_list_view_with_multiple_tasks(client, user, logged, goal_task, piece_task):
     """
-    Task list view provides all exisiting user tasks.
+    Task list view provides all existing user's tasks.
     """
     url = reverse('tasks:list', args=[user.username])
     response = client.get(url)
     assert response.status_code == 200
     assert response.context['task_list'].count() == 2
+
+@pytest.mark.django_db
+def test_task_list_view_provides_user_tasks_only(client, user, logged, user2, goal_task):
+    """
+    Task list view provides doesn't provide other users' tasks.
+    """
+    other_user_goal = Goal.objects.create(name="Koncert", user=user2)
+    other_user_task = Task.objects.create(goal=other_user_goal, user=user2)
+    url = reverse('tasks:list', args=[user.username])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert goal_task in response.context['task_list']
+    assert other_user_task not in response.context['task_list']
