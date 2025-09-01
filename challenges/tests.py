cd40d2946e5e895e8884d8e2e7ffc1629a36ee44
lukasz <lucapiasek@gmail.com>
2025-09-01 09:43:48 +0200

    test: add challenge list view provides users' challenge test

--- challenges/tests.py
+++ challenges/tests.py
@@ -11,3 +11,13 @@ def test_challenge_list_view(client, user, logged):
     assert response.status_code == 200
     template_names = [t.name for t in response.templates if t.name is not None]
     assert 'challenges/challenge_list.html' in template_names
+
+@pytest.mark.django_db
+def test_challenge_list_view_with_challenge(client, user, logged, goal_task_challenge):
+    """
+    Challenge list view provides users' challenge.
+    """
+    url = reverse('challenges:list', args=[user.username])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert goal_task_challenge in response.context['challenge_list']
