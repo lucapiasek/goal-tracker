1c4b1823a1f8eec0a669b79e796d9342beb7c3c5
lukasz <lucapiasek@gmail.com>
2025-09-01 09:21:18 +0200

    add user, logged, goal, piece and task fixtures in challenges app conftest

---
+++ challenges/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from django.test import Client
+from django.contrib.auth import get_user_model
+from tracker.models import Task, Practice, Goal, Piece
+from factory import Faker
+
+UserModel = get_user_model()
+
+@pytest.fixture
+def client():
+    return Client()
+
+@pytest.fixture
+def user():
+    return UserModel.objects.create_user(username="test", password="password")
+
+@pytest.fixture
+def logged(client, user):
+    client.force_login(user)
+
+@pytest.fixture
+def user2():
+    return UserModel.objects.create_user(username="test2", password="password")
+
+@pytest.fixture
+def goal(user):
+    return Goal.objects.create(user=user, name=Faker("sentence"))
+
+@pytest.fixture
+def piece(user):
+    return Piece.objects.create(user=user, name_to_display=Faker("name"))
+
+@pytest.fixture
+def goal_task(user, goal):
+    return Task.objects.create(user=user, goal=goal)
+
+@pytest.fixture
+def piece_task(user, piece):
+    return Task.objects.create(user=user, piece=piece)
