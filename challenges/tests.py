16c58fa0722d3f7dcc45060dc23c60fc555be9de
lukasz <lucapiasek@gmail.com>
2025-09-01 09:28:01 +0200

    test: add challenge list view test

--- challenges/tests.py
+++ challenges/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_challenge_list_view(client, user, logged):
+    """
+    Challenge list view exists and provides correct template
+    """
+    url = reverse('challenges:list', args=[user.username])
+    response = client.get(url)
+    assert response.status_code == 200
+    template_names = [t.name for t in response.templates if t.name is not None]
+    assert 'challenges/challenge_list.html' in template_names
